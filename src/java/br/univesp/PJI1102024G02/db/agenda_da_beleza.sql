f54a77d186928985478257e43c1a95ac6287f7fb
rubensanrodrigues <rubensan.rodrigues@gmail.com>
2024-04-15 18:11:17 -0300

    Adição do arquivo de estrutura inicial da base de dados

---
+++ src/java/br/univesp/PJI1102024G02/db/agenda_da_beleza.sql
@@ -0,0 +1,22 @@
+/**
+ * Author: rubensanrodrigues
+ * Created: 3 de abr. de 2024
+ */
+
+CREATE SCHEMA `agenda_da_beleza` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+
+USE `agenda_da_beleza`;
+
+CREATE USER agenda_user IDENTIFIED BY 'auser1234';
+
+GRANT USAGE ON *.* TO agenda_user@localhost IDENTIFIED BY 'auser1234';
+GRANT ALL PRIVILEGES ON agenda_da_beleza.* TO agenda_user@localhost;
+
+CREATE TABLE `agendamento` (
+  `contato` varchar(50) NOT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `observacao` varchar(200) DEFAULT NULL,
+  `horario` bigint(20) DEFAULT NULL,
+  `chaveacesso` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`chaveacesso`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
